2e8e2f5eb646ada6bbe8bcfc5ac35a8469919820
marcelbbarreiro <marcelbarreiro@gmail.com>
2022-06-07 19:24:00 +0200

    feat: render team

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,16 +1,59 @@
 
+
+
 async function displayTeam () {
-    const response = await fetch('../../resources/emoplyee.json'); //Obtener los datos del archivo json
+    const response = await fetch('../../resources/employees.json'); //Obtener los datos del archivo json
     const data = await response.json(); //Convertir los datos a json
     return data;
 }
 
-function renderTeam () {
+async function renderTeam () {
+    const cardsColumn = document.getElementById('cards-column');
     const teamPlayers = await displayTeam();
-    console.log(teamPlayers);
+    Array.from(teamPlayers).forEach(player => {
+        // cardsColumn.append(createCard(player));
+        console.log(cardsColumn);
+    })
 }
 
-// renderTeam();
+renderTeam();
 
-console.log(displayTeam());
+function createCard(playerData) {
+     const cardTeam = document.createElement('div');
+     cardTeam.classList.add('card_team');
+     const cardBodyPlayer = document.createElement('div');
+     cardBodyPlayer.classList.add('card-body-player');
+        cardTeam.append(cardBodyPlayer);
+     const player = document.createElement('div');
+     player.classList.add('player');
+        cardBodyPlayer.append(player);
+     const flag = document.createElement('div');
+     flag.classList.add('flag');
+        player.append(flag);
+    const playerInfo = document.createElement('div');
+    playerInfo.classList.add('player-info');
+        cardTeam.append(playerInfo);
+    const audio = document.createElement('div');
+    audio.classList.add('audio');
+        playerInfo.append(audio);
+        const icon = document.createElement('i');
+        icon.classList.add('fa-solid', 'fa-play', 'icon_audio');
+        audio.append(icon);
+    const playerName = document.createElement('div');
+    playerName.classList.add('player-name');
+        playerInfo.append(playerName);
+        playerName.textContent = `${playerData.name} ${playerData.lastname}`
+
+        console.log("Hola");
+    
+
+
+
+return card;
+
+}
+
+
+
+
 
